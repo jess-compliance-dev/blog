69e1e460d4c5a0d1fde15b1ad56ae47a7917a88d
jess-compliance-dev <jessica.kaschade@gmx.de>
2026-03-05 15:07:24 +0100

    designed data structure and created first route

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+def load_posts():
+    with open("posts.json", "r") as file:
+        posts = json.load(file)
+    return posts
+
+
+@app.route('/')
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
