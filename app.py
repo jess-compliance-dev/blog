ce70d82dd9fb8de440d96030d6e2c880b45e2584
jess-compliance-dev <jessica.kaschade@gmx.de>
2026-03-05 15:15:17 +0100

    created /add route and form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import json
 
 app = Flask(__name__)
@@ -6,14 +6,40 @@ app = Flask(__name__)
 
 def load_posts():
     with open("posts.json", "r") as file:
-        posts = json.load(file)
-    return posts
+        return json.load(file)
+
+
+def save_posts(posts):
+    with open("posts.json", "w") as file:
+        json.dump(posts, file, indent=4)
 
 
 @app.route('/')
 def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+
+        posts = load_posts()
+
+        new_post = {
+            "id": len(posts) + 1,
+            "author": request.form['author'],
+            "title": request.form['title'],
+            "content": request.form['content']
+        }
+
+        posts.append(new_post)
+
+        save_posts(posts)
+
+        return redirect('/')
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
